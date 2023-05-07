768abe8b38e48d28fede37d7ebedda20ec68cdfe
DavidKng <David_Kng@hotmail.com>
2023-05-06 21:30:26 -0300

    ultimos cambios

---
+++ memoryGame/my-project/src/Data.js
@@ -0,0 +1,53 @@
+
+export const imgs = [
+    {
+    id: 1 ,
+    img: "/img/facebook.png" ,
+    alt: "facebook"
+    },
+    {
+     id: 2,
+    img: "/img/discord.png",
+    alt: "discord"
+    },
+    {
+    id: 3 ,
+    img: "/img/google_duo.png",
+    alt: "google duo"
+    },
+    {
+    id: 4,
+    img: "/img/google.png",
+    alt: "google" 
+    },
+    {
+     id: 5,
+    img: "/img/instagram.png",
+    alt: "instagram"
+    },
+    {
+    id: 6,
+    img: "/img/linkedin.png",
+    alt: "linkedin"
+    },
+    {
+    id: 7,
+    img: "/img/messenger.png",
+    alt: "messenger"
+    },
+    {
+     id: 8,
+    img: "/img/netflix.png",
+    alt: "netflix"
+    },
+    {
+    id: 9,
+    img: "/img/spotify.png",
+    alt: "spotify"
+    },
+    {
+     id: 10,
+     img: "/img/whatsapp.png",
+     alt: "whatsapp"  
+    }          
+];
